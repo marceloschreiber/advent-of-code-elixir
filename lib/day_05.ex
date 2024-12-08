6690e07ff38a8fcba2fd4c804b7eb93c217b013e
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2024-12-07 22:19:43 -0300

    Day 05

--- lib/day_05.ex
+++ lib/day_05.ex
@@ -1,57 +1,59 @@
 defmodule AdventOfCode.Day05 do
   def part1(input) do
-    [map_str, lists_str] = String.split(input, "\n\n", trim: true)
+    {rules, updates} = parse_input(input)
 
-    order_map = build_order_map(map_str)
-
-    lists_str
-    |> parse_and_process_lists(order_map)
-    |> calculate_result()
-  end
-
-  defp build_order_map(map_str) do
-    map_str
-    |> String.split("\n", trim: true)
-    |> Enum.reduce(%{}, fn line, acc ->
-      [before, next] = String.split(line, "|", trim: true)
-      Map.update(acc, next, MapSet.new([before]), &MapSet.put(&1, before))
-    end)
-  end
-
-  defp parse_and_process_lists(lists_str, order_map) do
-    lists_str
-    |> String.split("\n", trim: true)
-    |> Enum.map(&process_number_list(&1, order_map))
-    |> Enum.filter(&(&1 != false))
-  end
-
-  defp process_number_list(line, order_map) do
-    numbers = String.split(line, ",", trim: true)
-
-    case check_numbers_validity(numbers, order_map) do
-      false -> false
-      _visited -> numbers
-    end
-  end
-
-  defp check_numbers_validity(numbers, order_map) do
-    numbers
-    |> Enum.reduce_while(MapSet.new(), fn number, visited ->
-      if MapSet.member?(visited, number) do
-        {:halt, false}
-      else
-        {:cont, MapSet.union(visited, Map.get(order_map, number, MapSet.new()))}
-      end
-    end)
-  end
-
-  defp calculate_result(valid_lists) do
-    valid_lists
-    |> Enum.map(fn numbers ->
-      middle_index = div(length(numbers), 2)
-      Enum.at(numbers, middle_index)
-    end)
-    |> Enum.map(&String.to_integer/1)
+    updates
+    |> Enum.filter(&sorted?(rules, &1))
+    |> Enum.map(&middle/1)
     |> Enum.sum()
+  end
+
+  def part2(input) do
+    {rules, updates} = parse_input(input)
+
+    updates
+    |> Enum.reject(&sorted?(rules, &1))
+    |> Enum.map(fn update ->
+      update
+      |> Enum.sort(&greater?(rules, &1, &2))
+      |> middle()
+    end)
+    |> Enum.sum()
+  end
+
+  defp middle(list) do
+    Enum.at(list, div(length(list), 2))
+  end
+
+  defp parse_input(input) do
+    [rules_raw, updates_raw] = String.split(input, "\n\n")
+
+    rules =
+      rules_raw
+      |> String.split(["|", "\n"], trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(2)
+      |> Enum.group_by(fn [a, _] -> a end, fn [_, b] -> b end)
+
+    updates =
+      updates_raw
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn report ->
+        report
+        |> String.split(",")
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    {rules, updates}
+  end
+
+  defp greater?(rules, greater, lesser) do
+    lesser in Map.get(rules, greater, [])
+  end
+
+  defp sorted?(rules, report) do
+    report
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [greater, lesser] -> greater?(rules, greater, lesser) end)
   end
 end
